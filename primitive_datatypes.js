2c3607bc88493c6010884c90f8e8d93e221a8aea
AnishJanani <139530339+AnishJanani@users.noreply.github.com>
2026-03-11 23:58:27 +0530

    Add files via upload
    This is an example program for primitive datatypes

---
+++ primitive_datatypes.js
@@ -0,0 +1,16 @@
+var firstName = "Anish.M"
+var companyName = `HPE`
+var mobileNumber = 9940952728
+var isAutomation = true
+
+console.log(firstName);
+console.log(typeof firstName);
+
+console.log(companyName);
+console.log(typeof companyName);
+
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+console.log(isAutomation);
+console.log(typeof isAutomation);
